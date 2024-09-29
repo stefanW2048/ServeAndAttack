b39ea78a26e7983eb8e8feca0e67004060fa444e
Wittmann <stefan.wittmann@teraport.de>
2024-09-29 10:50:34 +0200

    startup refactoring

--- init.js
+++ init.js
@@ -5,34 +5,53 @@ var App = App || {};
 (function () {
     App.init = {
         initializeApp: function () {
-            let team1Name = 'Lenting';
-            let team2Name = 'TV Ingolstadt';
+            let team1Name = 'Team 1';
+            let team2Name = 'Team 2';
 
-            const debug = true;
-            if (!debug) {
-                team1Name = prompt('Enter the name of Team 1:');
-                team2Name = prompt('Enter the name of Team 2:');
+            const debug = false;
+            if (debug) {
+                team1Name = 'Lenting';
+                team2Name = 'MTV';
             }
 
-            if (team1Name && team2Name && !(team1Name==team2Name)) {
-                App.models.teams = [
-                    {
-                        teamName: team1Name,
-                        players: [],
-                    },
-                    {
-                        teamName: team2Name,
-                        players: [],
-                    },
-                ];
+
+            App.models.teams = [
+                {
+                    teamName: team1Name,
+                    players: [],
+                },
+                {
+                    teamName: team2Name,
+                    players: [],
+                },
+            ];
+            App.models.servingTeamIndex = 0;
+            App.models.currentPlayerIndex = null;
+            App.ui.addTeamButtons();
+            App.ui.updateTeamButtons();
+            App.draw.drawCourt(); // Update the court to display the new team names
+
+        },
+
+        askTeamNames: function () {
+
+            if (App.models.teams[0].teamName != 'Team 1') {
+                return; //Team names already set
+            }
+            let team1Name = prompt('Enter the name of Team 1:');
+            let team2Name = prompt('Enter the name of Team 2:');
+
+
+            if (team1Name && team2Name && !(team1Name == team2Name) && team1Name!= 'Team 1') {
+                App.models.teams[0].teamName = team1Name;
+                App.models.teams[1].teamName = team2Name;
                 App.models.servingTeamIndex = 0;
                 App.models.currentPlayerIndex = null;
-                App.ui.addTeamButtons();
                 App.ui.updateTeamButtons();
                 App.draw.drawCourt(); // Update the court to display the new team names
             } else {
                 alert('Both team names are required.');
-                App.init.initializeApp(); // Restart initialization if names are not provided
+                App.init.askTeamNames(); // Restart initialization if names are not provided
             }
         },
     };
